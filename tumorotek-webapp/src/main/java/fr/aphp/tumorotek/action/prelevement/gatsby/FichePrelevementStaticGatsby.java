738e136c31439d8e71aa7f6b051359999bedc672
mbarthelemy <mathieu.barthelemy@php.fr>
2021-05-28 14:34:19 +0200

    programmatic hide item div

--- tumorotek-webapp/src/main/java/fr/aphp/tumorotek/action/prelevement/gatsby/FichePrelevementStaticGatsby.java
+++ tumorotek-webapp/src/main/java/fr/aphp/tumorotek/action/prelevement/gatsby/FichePrelevementStaticGatsby.java
@@ -3,11 +3,18 @@
  **/
 package fr.aphp.tumorotek.action.prelevement.gatsby;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.Div;
 import org.zkoss.zul.Groupbox;
 
 import fr.aphp.tumorotek.action.prelevement.FichePrelevementStatic;
 import fr.aphp.tumorotek.model.coeur.prelevement.Prelevement;
+import fr.aphp.tumorotek.webapp.gatsby.DisplayItem;
+import fr.aphp.tumorotek.webapp.gatsby.client.json.ChampEntite;
+import fr.aphp.tumorotek.webapp.gatsby.client.json.Contexte;
 
 /**
  *
@@ -30,18 +37,120 @@ public class FichePrelevementStaticGatsby extends FichePrelevementStatic
 
    private static final long serialVersionUID = -7612780578022559022L;
    
+   // Identifiants prélèvement
+   private Div identifiantBlockDiv;
    private Div codeDiv;
    private Div codeLaboDiv;
    private Div natureDiv;
+   
+   // /////
+   // group patient + maladie
+   // private Groupbox groupPatient; // géré dans classe parente 
+   
+   // Resume patient
+   private Div patientBlockDiv;
    private Div nipDiv;
    private Div ndaDiv;
    private Div nomDiv;
    private Div prenomDiv;
    private Div dateNaisDiv;
    private Div sexeDiv;
+   
+   // Maladie
+   // private Div linkMaladie; // géré dans classe parente
+   private Div libelleDiv;
+   private Div codeMaladieDiv;
+   
+   // //////
+   // group prelevement
+   // private Groupbox groupPrlvt; // géré dans classe parente
+   
+   // Informations prélèvement
+   private Div infoPrelBlockDiv;
+   private Div datePrelDiv;
+   private Div typeDiv;
+   private Div sterileDiv;
+   private Div risquesDiv;
+   private Div etabPreleveurDiv;
+   private Div servicePreleveurDiv;
+   private Div preleveurDiv;
+   
+   // Conditionnement
+   private Div conditBlockDiv;
+   private Div conditTypeDiv;
+   private Div conditNbrDiv;
+   private Div conditMilieuDiv;
+   
+   // Consentement
+   private Div consentBlockDiv;
+   private Div consentTypeDiv;
+   private Div consentDateDiv;
+   
+   // //////
+   // Transfert site preleveur vers site stockage
+   // private Groupbox gridFormPrlvtComp; // géré dans classe parente
+   
+   // Depart site preleveur
+   private Div departBlockDiv;
+   private Div dateDepartDiv;
+   private Div transporteurDiv;
+   private Div tempTranspDiv;
+   private Div congPrelDiv;
+   
+   // Sites d'analyse / Labos intermédiaires
+   // private Div groupLaboInter; // géré dans classe parente
+   
+   // Arrivee site stockage
+   private Div arriveeBlockDiv;
+   private Div dateArriveeDiv;
+   private Div operateurDiv;
+   private Div quantiteDiv;
+   private Div conformeArriveeDiv;
+   private Div congBiothequeDiv;
+   
+   // //////
+   // Echantillons
+   // private Groupbox groupEchans; // géré dans classe parente
 
+   // //////
+   // Dérives
+   // private Groupbox groupDerivesPrlvt; // géré dans classe parente
    
    @Override
+   public void doAfterCompose(final Component comp) throws Exception{
+      super.doAfterCompose(comp);
+      
+      List<Div> divs = new ArrayList<Div>();
+      divs.add(codeDiv);
+      divs.add(codeLaboDiv);
+      divs.add(natureDiv);
+
+      // test only
+      Contexte currentContexte = mockOneContexte();
+    
+      DisplayItem.showOrhideItems(divs, currentContexte);
+   }
+   
+   
+   private Contexte mockOneContexte() {
+	   Contexte cont = new Contexte();
+	   ChampEntite code = new ChampEntite();
+	   code.setChampId(23);
+	   code.setVisible(true);
+	   cont.getChampEntites().add(code);
+	   ChampEntite codeLabo = new ChampEntite();
+	   codeLabo.setChampId(45);
+	   codeLabo.setVisible(false);
+	   cont.getChampEntites().add(codeLabo);
+	   ChampEntite nature = new ChampEntite();
+	   nature.setChampId(24);
+	   nature.setVisible(false);
+	   cont.getChampEntites().add(nature);
+	   return cont;
+   }
+
+
+@Override
    protected void disablePatientGroup() {
 	   boolean enable = (this.maladie != null || this.prelevement.equals(new Prelevement()));
 	   ((Groupbox) this.groupPatient).setOpen(enable);
