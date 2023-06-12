2bc6aeedaec12f39be4de9d89fc8fef336028f1e
Pini <pini.tzr@gmail.com>
2023-06-12 09:58:19 +0200

    correction

--- tumorotek-core/src/main/java/fr/aphp/tumorotek/manager/impl/interfacage/PatientSipManagerImpl.java
+++ tumorotek-core/src/main/java/fr/aphp/tumorotek/manager/impl/interfacage/PatientSipManagerImpl.java
@@ -200,9 +200,8 @@ public class PatientSipManagerImpl implements PatientSipManager
             if(pats.size() == 1){
                return pats.get(0);
             }else{ // message non traité
-               log.error("Synchronization SIP impossible! Plusieurs patients sont enregistrés dans le système " +
-                  "pour ce nip {}", sipPatient.getNip());
-
+               log.error("Synchronization SIP impossible! Plusieurs patients sont enregistrés dans le système pour ce nip {}",
+                  sipPatient.getNip());
             }
          }
       }
