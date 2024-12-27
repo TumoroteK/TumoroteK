60350f5c4e572d4537eee1f8b1dffae412ab03c0
2snufkin <pini.tzr@gmail.com>
2024-12-27 12:08:56 +0100

    tooltip and space

--- tumorotek-webapp/src/main/java/fr/aphp/tumorotek/action/echantillon/FicheEchantillonStatic.java
+++ tumorotek-webapp/src/main/java/fr/aphp/tumorotek/action/echantillon/FicheEchantillonStatic.java
@@ -303,8 +303,7 @@ public class FicheEchantillonStatic extends AbstractFicheStaticController
       initDelaiCgl();
       // Récupère la liste des retours associés à l'échantillon ayant un impact (dégradation probable de la qualité du matériel).
       List<Retour>retours = ManagerLocator.getRetourManager().findByObjectAndImpactManager(echantillon, true);
-      // Définit une info-bulle pour l'icône d'impact indiquant la présence de retours avec impact.
-      impactIcon.setTooltiptext(Labels.getLabel("Champ.Retour.Impact"));
+
       // Rend l'icône d'impact visible uniquement si des retours avec impact sont trouvés.
       impactIcon.setVisible(!retours.isEmpty());
 
