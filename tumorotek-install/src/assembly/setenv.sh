b2c2c9bc9f6f011463ea3c22f3dc2b33c8ba2d43
HUET-Sesan <94914012+HUET-Sesan@users.noreply.github.com>
2025-01-16 15:25:50 +0100

    cas particulier des Redhat

--- tumorotek-install/src/assembly/setenv.sh
+++ tumorotek-install/src/assembly/setenv.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Attention : pour les tomcats de la distribution Redhat (ex : CHU Tours, HCL) les paramètres de démarrage doivent être définis dans le fichier .../conf/tomcat.conf, avec la clé CATALINA_OPTS :
+# CATALINA_OPTS="-Xms512m -Xmx1024m -Duser.language=en -Dlogback.configurationFile=$CATALINA_BASE/conf/Catalina/localhost/logback/logback.xml"
+# et ce fichier setenv.sh ne doit pas être déposé dans .../bin
 export CATALINA_OPTS="$CATALINA_OPTS -Xms512m -Xmx1024m -Duser.language=en -Dlogback.configurationFile=$CATALINA_BASE/conf/Catalina/localhost/logback/logback.xml"
